7fc5df7e299752dad488e60dea87490ab8cdffdd
Ina Singh <ina.singh@absolutelabs.co>
2026-05-08 18:18:23 +0530

    Fixed query client router context

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { QueryClient } from "@tanstack/react-query";
+
 import { routeTree } from "./routeTree.gen";
 
 import "./styles.css";
 
-const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
+
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
